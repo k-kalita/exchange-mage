8878c61d60f786dbd76a16aa1583b9451f866c10
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2023-11-06 13:58:44 +0100

    Implemented the Trigger interface and ConstValueTrigger class.
    TRIGGERS EMB-9 [OPEN] -> [IN PROGRESS]
    Trigger EMB-15 [OPEN] -> [DONE]
    ConstValueTrigger EMB-16 [OPEN] -> [DONE]

--- src/main/java/exchangemage/effects/triggers/Trigger.java
+++ src/main/java/exchangemage/effects/triggers/Trigger.java
@@ -1,5 +1,24 @@
 package exchangemage.effects.triggers;
 
+import exchangemage.effects.base.Effect;
+import exchangemage.effects.base.EffectPlayer;
+
+/**
+ * Triggers act as evaluators of certain requirements which must be met in order for an
+ * {@link Effect} to be enqueued into the resolution queue of the {@link EffectPlayer} and/or
+ * be resolved and executed.
+ *
+ * @see Effect
+ * @see EffectPlayer
+ */
 public interface Trigger {
+    /**
+     * Returns whether the {@link Trigger} is activated (i.e. whether the requirements of the
+     * trigger are met).
+     *
+     * @return <code>true</code> if the trigger is activated, <code>false</code> otherwise.
+     *
+     * @see Effect
+     */
     boolean isActivated();
 }
