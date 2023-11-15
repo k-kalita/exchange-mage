711cb9d900955591bef1b824fc64ff34bd308431
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2023-11-15 15:16:23 +0100

    Targetable interface implemented.
    Targetable interface extending the Observable interface to enable notifying observers of targeting-specific events written and documented.
    
    TARGETING EMB-10 [IN PROGRESS]
    Targetable EMB-25 [OPEN] -> [DONE]

--- src/main/java/exchangemage/effects/targeting/Targetable.java
+++ src/main/java/exchangemage/effects/targeting/Targetable.java
@@ -1,5 +1,104 @@
 package exchangemage.effects.targeting;
 
-public interface Targetable {
-    public boolean isSelected();
+import exchangemage.base.Observable;
+import exchangemage.base.Observer;
+import exchangemage.effects.base.Effect;
+
+/**
+ * An interface for all objects that can be targeted by {@link Effect}s. All elements of the game
+ * which can become targets of effects (such as <i>cards</i> or <i>actors</i>) should implement
+ * this interface.
+ * <br><br>
+ * This interface extends the {@link Observable} interface and defines a set of
+ * {@link TargetableEvent}s so that {@link Observer}s can be notified of events specific to
+ * targetable objects.
+ *
+ * @see Effect
+ * @see Observable
+ * @see Observer
+ */
+public interface Targetable extends Observable {
+    /**
+     * An enum defining events specific to {@link Targetable} objects. Used to notify
+     * {@link Observer}s of the objects implementing this interface.
+     *
+     * @see Observable
+     * @see Observer
+     */
+    enum TargetableEvent implements Observable.Event {
+        /**
+         * Event used to notify {@link Observer}s that the {@link Targetable} object can potentially
+         * be selected for the {@link Effect} whose target is currently being chosen.
+         *
+         * @see Observer
+         * @see Effect
+         */
+        ACTIVATED,
+        /**
+         * Event used to notify {@link Observer}s that the {@link Targetable} object can no longer
+         * be selected by the {@link Effect} whose target is currently being chosen.
+         *
+         * @see Observer
+         * @see Effect
+         */
+        DEACTIVATED,
+        /**
+         * Event used to notify {@link Observer}s that the {@link Targetable} object has been
+         * selected for the {@link Effect} whose target is currently being chosen.
+         *
+         * @see Observer
+         * @see Effect
+         */
+        SELECTED,
+        /**
+         * Event used to notify {@link Observer}s that the {@link Targetable} object has been
+         * deselected as the target for the {@link Effect} whose target is currently being chosen.
+         */
+        DESELECTED;
+    }
+
+    /**
+     * Notifies all {@link Observer}s of this {@link Targetable} object that the object has been
+     * activated (can be selected for the {@link Effect} whose target is currently being chosen).
+     *
+     * @see Observer
+     * @see Effect
+     */
+    public default void activate() {
+        notifyObservers(TargetableEvent.ACTIVATED);
+    }
+
+    /**
+     * Notifies all {@link Observer}s of this {@link Targetable} object that the object has been
+     * deactivated (can no longer be selected for the {@link Effect} whose target is currently
+     * being chosen).
+     *
+     * @see Observer
+     * @see Effect
+     */
+    public default void deactivate() {
+        notifyObservers(TargetableEvent.DEACTIVATED);
+    }
+
+    /**
+     * Notifies all {@link Observer}s of this {@link Targetable} object that the object has been
+     * selected for the {@link Effect} whose target is currently being chosen.
+     *
+     * @see Observer
+     * @see Effect
+     */
+    public default void select() {
+        notifyObservers(TargetableEvent.SELECTED);
+    }
+
+    /**
+     * Notifies all {@link Observer}s of this {@link Targetable} object that the object has been
+     * deselected as the target for the {@link Effect} whose target is currently being chosen.
+     *
+     * @see Observer
+     * @see Effect
+     */
+    public default void deselect() {
+        notifyObservers(TargetableEvent.DESELECTED);
+    }
 }
