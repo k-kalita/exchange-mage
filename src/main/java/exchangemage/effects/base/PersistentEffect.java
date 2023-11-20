cbf91b85c812cf4f7fafaa89a1ea1d906e9c2e7e
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2023-11-20 23:29:25 +0100

    PersistentEffect class updated.
    PersistentEffect updated to accommodate the new resolution logic, as well as some minor internal changes.
    
    EFFECTS EMB-8 [IN PROGRESS]
    PersistentEffect EMB-13 [PAUSED] -> [IN PROGRESS]

--- src/main/java/exchangemage/effects/base/PersistentEffect.java
+++ src/main/java/exchangemage/effects/base/PersistentEffect.java
@@ -5,6 +5,9 @@ import exchangemage.effects.targeting.TargetSelector;
 import exchangemage.effects.triggers.Trigger;
 import exchangemage.effects.triggers.ConstValueTrigger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * An {@link Effect} which represents a persistent environmental/actor-specific effect, storing
  * within it effects which can be activated by external occurrences (e.g. effects which activate
@@ -12,31 +15,48 @@ import exchangemage.effects.triggers.ConstValueTrigger;
  * <br><br>
  * Persistent effects can be activated during the resolution of other effects. The order in which
  * they are activated is determined by their assigned {@link EffectPlayer.EffectResolutionStage}.
- * Upon execution, a persistent effect will enqueue all of its effects into the resolution queue
- * of the {@link EffectPlayer}.
+ * Upon execution, a persistent effect will call the {@link EffectPlayer#evaluateEffect} method
+ * of the {@link EffectPlayer} to evaluate the resolution of all its stored effects.
  *
  * @see Effect
  * @see EffectPlayer
  * @see EffectPlayer.EffectResolutionStage
  */
 public class PersistentEffect extends Effect {
-    private final Effect[] effects;
+    /**
+     * {@link Effect}s which will be deployed upon the activation of the {@link PersistentEffect}.
+     */
+    private final List<Effect> effects;
+    /**
+     * The activation stage of the {@link PersistentEffect}. Dictates the stage of the
+     * {@link Effect} resolution process in which the persistent effect can be activated.
+     */
     private final EffectPlayer.EffectResolutionStage activationStage;
 
     /**
-     * Constructs a {@link PersistentEffect} with given {@link Effect}s, activation stage and
-     * {@link TargetSelector}.
+     * Constructs a {@link PersistentEffect} with given stored {@link Effect}s, activation stage,
+     * {@link Trigger}s, and {@link TargetSelector}.
      *
-     * @param effects effects of the persistent effect
+     * @param effects effects stored within the PersistentEffect
      * @param activationStage activation stage of the persistent effect
      * @param targetSelector target selector of the persistent effect
+     *
+     * @see Effect
+     * @see EffectPlayer.EffectResolutionStage
+     * @see Trigger
+     * @see TargetSelector
      */
-    public PersistentEffect(Effect[] effects,
+    public PersistentEffect(List<Effect> effects,
                             EffectPlayer.EffectResolutionStage activationStage,
                             Trigger activationTrigger,
                             TargetSelector targetSelector) {
-        super(activationTrigger, new ConstValueTrigger(true), targetSelector);
-        this.effects = effects;
+        super(
+                activationTrigger,
+                new ConstValueTrigger(true),
+                targetSelector,
+                ResolutionMode.IMMEDIATE
+        );
+        this.effects = new ArrayList<>(effects);
         this.activationStage = activationStage;
     }
 
@@ -51,19 +71,14 @@ public class PersistentEffect extends Effect {
     public EffectPlayer.EffectResolutionStage getActivationStage() { return activationStage; }
 
     /**
-     * Calls the {@link EffectPlayer#enqueueEffect(Effect)} method of the
-     * {@link EffectPlayer} for each of its stored {@link Effect}s. This will result in all the
-     * effects whose activation {@link Trigger}s are activated being enqueued into the resolution
-     * queue.
+     * Calls the {@link EffectPlayer#evaluateEffect} method of the {@link EffectPlayer} for each
+     * of its stored {@link Effect}s.
      *
      * @see EffectPlayer
      * @see PersistentEffect
      */
     @Override
     public void execute() {
-        EffectPlayer effectPlayer = Game.getGame().getScene().getEffectPlayer();
-
-        for (Effect effect : effects)
-            effectPlayer.enqueueEffect(effect);
+        effects.forEach(effect -> EffectPlayer.getEffectPlayer().evaluateEffect(effect));
     }
 }
