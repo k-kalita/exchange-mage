1a5afff32bce54220e99fa4446f757092ffb2b3f
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2023-12-12 11:04:58 +0100

    Implement Condition interface and ConditionalTrigger class
    TRIGGERS EMB-9 [IN PROGRESS]
    Condition EMB-36 [OPEN] -> [DONE]
    ConditionalTrigger EMB-35 [OPEN] -> [AWAITING TESTING]

--- src/main/java/exchangemage/effects/triggers/Condition.java
+++ src/main/java/exchangemage/effects/triggers/Condition.java
@@ -1,4 +1,27 @@
 package exchangemage.effects.triggers;
 
+import exchangemage.base.GameState;
+import exchangemage.effects.base.Effect;
+import exchangemage.effects.base.PersistentEffect;
+import exchangemage.actors.Player;
+import exchangemage.actors.Actor;
+
+/**
+ * Base class for all conditions. Conditions represent statements about the current game state
+ * which can be evaluated to a boolean value. As part of {@link ConditionalTrigger}s they can be
+ * used to determine whether a given {@link Effect} should be triggered based on criteria such as
+ * the {@link Player}'s health, the statuses affecting the targeted {@link Actor}, the type of the
+ * effect currently in resolution (in case of {@link PersistentEffect}s' activation), etc.
+ *
+ * @see ConditionalTrigger
+ * @see GameState
+ * @see Effect
+ */
 public interface Condition {
+    /**
+     * Evaluates the statement represented by this condition to a boolean value.
+     *
+     * @return <code>true</code> if the condition is fulfilled, <code>false</code> otherwise
+     */
+    public boolean isFulfilled();
 }
---
+++ src/main/java/exchangemage/effects/triggers/ConditionalTrigger.java
@@ -0,0 +1,41 @@
+package exchangemage.effects.triggers;
+
+import java.util.Objects;
+
+import exchangemage.effects.base.Effect;
+
+/**
+ * A {@link Trigger} which is activated if and only if a given {@link Condition} is fulfilled.
+ * <br><br>
+ * Used by {@link Effect}s which trigger based on criteria pertaining to the current game state.
+ *
+ * @see Trigger
+ * @see Condition
+ */
+public class ConditionalTrigger implements Trigger {
+    /**
+     * The condition to be checked when evaluating activation of this trigger.
+     */
+    private final Condition condition;
+
+    /**
+     * Creates a new {@link ConditionalTrigger} with given {@link Condition}.
+     *
+     * @param condition the condition to be checked when evaluating activation
+     * @throws NullPointerException if the condition is null
+     */
+    public ConditionalTrigger(Condition condition) {
+        Objects.requireNonNull(condition, "Trigger condition cannot be null.");
+        this.condition = condition;
+    }
+
+    /**
+     * Evaluates whether the {@link Condition} of this trigger is fulfilled.
+     *
+     * @return <code>true</code> if the condition is fulfilled, <code>false</code> otherwise
+     */
+    @Override
+    public boolean isActivated() {
+        return condition.isFulfilled();
+    }
+}
