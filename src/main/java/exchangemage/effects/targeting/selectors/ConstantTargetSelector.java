f7d8450562ff2c0aa50329def630fcbac2b3f19f
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2024-01-12 02:30:54 +0100

    Update ConstantTargetSelector to use subject getters parametrized to types extending Targetable
    TARGETING EMB-10 [IN PROGRESS]
    ConstantTargetSelector EMB-19 [UPDATES NEEDED] -> [AWAITING TESTING]

--- src/main/java/exchangemage/effects/targeting/selectors/ConstantTargetSelector.java
+++ src/main/java/exchangemage/effects/targeting/selectors/ConstantTargetSelector.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 import java.util.Set;
 
 import exchangemage.effects.Effect;
-import exchangemage.effects.targeting.TargetGetter;
 import exchangemage.effects.targeting.Targetable;
+import exchangemage.effects.triggers.conditions.getters.SubjectGetter;
 
 /**
  * A {@link TargetSelector} which always selects the same target for its {@link Effect}
@@ -20,21 +20,21 @@ public class ConstantTargetSelector<T extends Targetable> extends TargetSelector
     /**
      * Functional interface used to return the target of the {@link ConstantTargetSelector}.
      */
-    private final TargetGetter<T> targetGetter;
+    private final SubjectGetter<T> targetGetter;
 
     /**
-     * Creates a new {@link ConstantTargetSelector} with given {@link TargetGetter} and target
+     * Creates a new {@link ConstantTargetSelector} with given {@link SubjectGetter} and target
      * class.
      *
-     * @param targetGetter implementation of the {@link TargetGetter} functional interface used to
+     * @param targetGetter implementation of the {@link SubjectGetter} functional interface used to
      *                     return this {@link TargetSelector}'s target
      * @param targetClass  the class of the {@link Targetable} objects selected by this selector
      * @throws NullPointerException if the given target getter is <code>null</code>
-     * @see TargetGetter
+     * @see SubjectGetter
      * @see TargetSelector
      * @see Targetable
      */
-    public ConstantTargetSelector(TargetGetter<T> targetGetter, Class<T> targetClass) {
+    public ConstantTargetSelector(SubjectGetter<T> targetGetter, Class<T> targetClass) {
         super(targetClass);
         Objects.requireNonNull(
                 targetGetter,
@@ -44,41 +44,41 @@ public class ConstantTargetSelector<T extends Targetable> extends TargetSelector
     }
 
     /**
-     * Chooses the target returned by the {@link TargetGetter#getTarget()} method if the provided
+     * Chooses the target returned by the {@link SubjectGetter#getSubject()} method if the provided
      * set of forbidden targets does not contain it.
      *
      * @param forbiddenTargets the set of forbidden targets to exclude from the selection process
      * @return <code>true</code> if the target choosing process was successful, <code>false</code>
      * otherwise
      * @throws NullPointerException   if the given set of forbidden targets is <code>null</code>
-     * @see TargetGetter
+     * @see SubjectGetter
      * @see Targetable
      */
     @Override
     public boolean selectTarget(Set<Targetable> forbiddenTargets) {
         Objects.requireNonNull(forbiddenTargets, "Forbidden targets cannot be null.");
 
-        if (forbiddenTargets.contains(this.targetGetter.getTarget()))
+        if (forbiddenTargets.contains(this.targetGetter.getSubject()))
             return false;
 
-        this.target = this.targetGetter.getTarget();
+        this.target = this.targetGetter.getSubject();
         return true;
     }
 
     /**
      * Validates the given {@link Targetable} object by checking if it is the same as the target
-     * returned by the {@link TargetGetter#getTarget()} method of this selector's
-     * {@link TargetGetter}.
+     * returned by the {@link SubjectGetter#getSubject()} ()} method of this selector's
+     * {@link SubjectGetter}.
      *
      * @param target the target to validate.
      * @throws InvalidTargetException if the given target does not match the target returned by
      *                                the TargetGetter.
-     * @see TargetGetter
+     * @see SubjectGetter
      * @see Targetable
      */
     @Override
     protected void validateTarget(T target) {
-        if (target != this.targetGetter.getTarget())
+        if (target != this.targetGetter.getSubject())
             throw new InvalidTargetException(
                     "Invalid target for constant target selector. Does not match target getter."
             );
